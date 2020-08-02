da4360cece8ae14576fac780365c0b28b4c13b7d
whxcctv <whxcctv@gmail.com>
2020-08-02 22:23:48 +0800

    files structure

--- examples/fspromise_test.js
+++ examples/fspromise_test.js
@@ -1,29 +1,7 @@
 const fs = require('fs');
 const {Promise} = require('../index');
 
-var readFile=function (file,options) {
-  return new Promise(function (s,f) {
-    fs.readFile(file,options,function (e,d) {
-      if(e){
-        f(e);
-        return
-      }
-      s(d);
-    })
-  })
-}
 
-var writeFile=function (file,data,options) {
-  return new Promise(function (s,f) {
-    fs.writeFile(file,data,options,function (e) {
-      if(e){
-        f(e);
-        return
-      }
-      s();
-    })
-  })
-}
 function err(e) {
   console.log(e);
 }
